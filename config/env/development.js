22361a562270dc1b21ecae251a0323d0b08dab28
Dolnapa <smile_yingying@hotmail.com>
2017-09-23 12:11:22 +0700

    up

---
+++ config/env/development.js
@@ -0,0 +1,8 @@
+module.exports = {
+       mongoUri: 'mongodb://localhost/myFirstNode',
+       // or use 
+       // mongoUri: 'mongodb://admin:password@localhost:27017/myFirstNode',
+       mongoUri: 'mongodb://admin1:admin1@localhost:27017/testDB1',
+       debug: true,
+       sessionSecret: 'dev_secret_key'
+}
