a38779a806fcbe2c2337b618f164534def3dc01e
smileyingg <31153961+smileyingg@users.noreply.github.com>
2017-10-07 16:23:59 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import mongoose from './config/mongoose';
 import express from './config/express';
+import passport from './config/passport';
+
 
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
@@ -7,6 +9,7 @@ process.env.PORT = process.env.PORT || 3000;
 
 var db = mongoose();
 var app = express();
+var psp = passport();
 
 app.listen(process.env.PORT, () => {
        console.log('Starting node.js on port ' + process.env.PORT);
